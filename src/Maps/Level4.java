494d5fac8eb5af9390fcebe5e2233962ddc4b611
JaidenPalazzo <jaiden.palazzo@quinnipiac.edu>
2025-10-23 18:15:37 -0400

    move enemies to be visible and be in arcade

--- src/Maps/Level4.java
+++ src/Maps/Level4.java
@@ -5,10 +5,15 @@ import EnhancedMapTiles.EndLevelBox;
 import Level.*;
 import NPCs.Chest;
 import Tilesets.CommonTileset;
+import Utils.Direction;
+
 //import Utils.Direction;
 import java.util.ArrayList;
 //import Engine.ImageLoader;
 //import GameObject.Rectangle;
+
+import Enemies.RangeEnemy;
+import Enemies.SwordPirate;
 
 public class Level4 extends Map{
 
@@ -18,18 +23,34 @@ public class Level4 extends Map{
     }
     
 
-@Override
-public ArrayList<Enemy> loadEnemies() {
-    ArrayList<Enemy> enemies = new ArrayList<>();
+// @Override
+// public ArrayList<Enemy> loadEnemies() {
+//     ArrayList<Enemy> enemies = new ArrayList<>();
 
-   // BugEnemy bugEnemy = new BugEnemy(getMapTile(16, 10).getLocation().subtractY(25), Direction.LEFT);
-   // enemies.add(bugEnemy);
+//    // BugEnemy bugEnemy = new BugEnemy(getMapTile(16, 10).getLocation().subtractY(25), Direction.LEFT);
+//    // enemies.add(bugEnemy);
 
-   // DinosaurEnemy dinosaurEnemy = new DinosaurEnemy(getMapTile(19, 1).getLocation().addY(2), getMapTile(22, 1).getLocation().addY(2), Direction.RIGHT);
-   // enemies.add(dinosaurEnemy);
+//    // DinosaurEnemy dinosaurEnemy = new DinosaurEnemy(getMapTile(19, 1).getLocation().addY(2), getMapTile(22, 1).getLocation().addY(2), Direction.RIGHT);
+//    // enemies.add(dinosaurEnemy);
 
-    return enemies;
-}
+//     return enemies;
+// }
+
+   public ArrayList<Enemy> loadEnemies() {
+        ArrayList<Enemy> enemies = new ArrayList<>();
+
+       SwordPirate swordDude = new SwordPirate(getMapTile(10, 17).getLocation().subtractY(25),getMapTile(18, 10).getLocation().subtractY(25), Direction.LEFT);
+       //getMapTile(16, 10).getLocation().subtractY(25)
+       enemies.add(swordDude);
+
+        RangeEnemy rangedPirate = new RangeEnemy(getMapTile(19, 1).getLocation().addY(2), getMapTile(22, 1).getLocation().addY(2), Direction.RIGHT);
+        enemies.add(rangedPirate);
+
+        //DinosaurEnemy SwordPirate = new DinosaurEnemy(getMapTile(19, 1).getLocation().addY(2), getMapTile(22, 1).getLocation().addY(2), Direction.RIGHT);
+       // enemies.add(SwordPirate);
+
+        return enemies;
+    }
 
 @Override
 public ArrayList<EnhancedMapTile> loadEnhancedMapTiles() {
